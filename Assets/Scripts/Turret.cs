da959a9f31d24e807bb0c84754e4463cac26e696
van1ove <berezko2004@gmail.com>
2023-12-23 00:58:15 +0300

    created turret script

---
+++ Assets/Scripts/Turret.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Unity.VisualScripting;
+using UnityEngine;
+using Zenject;
+
+namespace DefaultNamespace
+{
+    public class Turret : MonoBehaviour
+    {
+        [Header("Stats")]
+        [SerializeField] private float rotationSpeed;
+        [SerializeField] private int damage;
+        [SerializeField] private int shootsPerMinute;
+        [SerializeField] private int health;
+        
+        public float RotationSpeed => rotationSpeed;
+        public int Damage => damage;
+        public int ShootsPerMinute => shootsPerMinute;
+        public int Health => health;
+
+        private float _time;
+        private float _shootDelay;
+
+        private void Start()
+        {
+            _time = 0;
+            _shootDelay = 60f / shootsPerMinute;
+        }
+
+        private void Update()
+        {
+            _time += Time.deltaTime;
+            if (_time <= _shootDelay) return;
+
+            Shoot();
+            _time = 0;
+            
+        }
+        
+        void Shoot()
+        {
+            
+        }
+        
+        void TakeDamage(int takenDamage)
+        {
+            if (health > 0)
+            {
+                health -= takenDamage;
+                if (health <= 0) ;
+            }
+        }
+
+
+    }
+}
